e2961c65fe19550582b7023dff8bd3659d30082e
belova_oa <belovva.o@gmail.com>
2021-10-28 19:05:25 +0300

    added TextBoxTests

---
+++ src/test/java/demo/qa/TextBoxTests.java
@@ -0,0 +1,20 @@
+package demo.qa;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+    @Test
+    void fillFormTest() {
+       open("https://demoqa.com/automation-practice-form");
+       $("[id=firstName]").setValue("Name1");
+       $("#lastName").setValue("Last1");
+       $("#userEmail").setValue("last1@ya.ru");
+
+
+
+
+    }
+}
